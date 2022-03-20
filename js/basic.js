0c16f9e5abc206a89d64ce634d1bc2b7ee2b372f
Munendracv <munendracv610@gmail.com>
2022-03-20 08:12:32 +0530

    it is printing undefined instead of string ?

--- js/basic.js
+++ js/basic.js
@@ -1,6 +1,15 @@
-window.addEventListener('load', onloaded);
-function onloaded(){
-let name;// var decliration
-name =sessionStorage.getItem('username')
-document.getElementById('name').innerHTML=name;
-}
+// window.addEventListener('load', onloaded);
+// function onloaded(){
+// let name;// var decliration
+// name =sessionStorage.getItem('username')
+// document.getElementById('name').innerHTML=name;
+// }
+//string,number,boolean,null,undefined
+const name ="john";
+const age = 33;
+const float = 4.5;
+const isCool = true;
+const x =null;
+const y = undefined;
+let z;
+console.log(typeof john);
